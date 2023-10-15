35eb3e7358c474148ba37b8affeff0401bcf6f99
PragatiTiwari123 <pragatip963@gmail.com>
2023-10-15 18:23:03 +0530

    Add Print Array function and Rotate Array code

--- Array2.java
+++ Array2.java
@@ -8,7 +8,31 @@ import java.util.*;
 //Do it for k times to do rotation k times
 
 public class Array2 {
+    public static void printArr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
     public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int arr[]={1,2,3,4,5};
+        int k=sc.nextInt();
+        int end=arr.length-1;
+        printArr(arr);
+        System.out.println();
 
+        while(k>0){
+            int i=0;
+            int n=0;
+            int a=arr[i];
+            while(n<arr.length-1){
+                arr[n]=arr[n+1];
+                n++;
+            }
+            i++;
+            k--;
+            arr[end]=a;
+        }
+        printArr(arr);
     }
 }
