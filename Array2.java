0b997c249412147287766e76e567f129b3cfd5f0
PragatiTiwari123 <pragatip963@gmail.com>
2023-10-15 17:15:36 +0530

    Add comments a breif description

--- Array2.java
+++ Array2.java
@@ -1,7 +1,14 @@
 package Java_DSA;
+import java.util.*;
+
+//In this program, We want to rotate the array by k times..
+//We are going to stire the first index value
+//Then swap the adjacent elements
+//and at last insert the value of first index into last index
+//Do it for k times to do rotation k times
 
 public class Array2 {
     public static void main(String []args){
-        
+
     }
 }
