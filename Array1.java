8126d7d465151b07ea5c7d1a000d91bc69f4a355
PragatiTiwari123 <pragatip963@gmail.com>
2023-10-15 16:57:02 +0530

    Update Function

--- Array1.java
+++ Array1.java
@@ -47,14 +47,54 @@ class Array1{
             return(arr[arr.length-2]);
         }
     }
+    public static int sLargest(Integer arr[]){
+        int end=arr.length-1;
+        Arrays.sort(arr);
+        printArr(arr);
+        int a1=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<0){
+                a1++;
+            }
+        }
+        // System.out.println(a1+"=="+(arr.length-1));
+        if(a1==end+1 && a1!=0){
+            int start=0;
+            for(int i=0;i<end;i++){
+                if(arr[start]<arr[start+1]){
+                    return arr[start+1];
+                }
+                else{
+                    start++;
+                }
+            }
+            return -1;
+        }
+        else{
+            for(int i=0;i<end;i++){
+                if(arr[end]>arr[end-1]){
+                    return arr[end-1];
+                }
+                else{
+                    end=end-1;
+                }
+            }
+            return -1;
+        }
+        
+        
+        
+    }
     public static void main(String []args){
         Scanner sc=new Scanner(System.in);
         int n_arr=sc.nextInt();
         while(n_arr>0){
+            System.out.print("Enter the no Of elements : ");
             int n=sc.nextInt();
             Integer arr[]=new Integer[n];
             create_arr(arr,n);
-            System.out.println("\n"+secondLargest(arr));
+            System.out.println("\n"+sLargest(arr));
+
             n_arr--;
         }
     }
